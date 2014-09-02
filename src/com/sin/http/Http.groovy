ccf59105c7f31029d2af4a42d072489725f71b8b
RobinTang <sintrb@gmail.com>
2014-09-02 17:02:28 +0800

    jceentity

--- src/com/sin/http/Http.groovy
+++ src/com/sin/http/Http.groovy
@@ -1,13 +1,19 @@
 package com.sin.http
 
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.*
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-def Get(url){
-	hc = HttpClients.createDefault()
-	get = new HttpGet(url)
-	res = hc.execute(get)
-	InputStream is = res.getEntity().getContent()
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.AbstractHttpEntity
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.InputStreamEntity
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClients
+
+
+def StreamToString(is){
 	BufferedReader br = new BufferedReader(new InputStreamReader(is))
 	sb = new StringBuffer()
 	l = br.readLine()
@@ -15,7 +21,72 @@ def Get(url){
 		sb.append(l)
 		l = br.readLine()
 	}
+	is.close()
 	return sb.toString()
 }
 
-//println Get("https://api.douban.com/v2/user/~me")
+def Execute(req){
+	hc = HttpClients.createDefault()
+	res = hc.execute(req)
+	return res.getEntity().getContent()
+}
+
+def Get(url){
+	get = new HttpGet(url)
+	return Execute(get)
+}
+
+class JCEEntity extends AbstractHttpEntity{
+
+	@Override
+	public InputStream getContent() throws IOException, IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getContentLength() {
+		// TODO Auto-generated method stub
+		return 8;
+	}
+
+	@Override
+	public boolean isRepeatable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isStreaming() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void writeTo(OutputStream os) throws IOException {
+		os.write(1);
+		os.write(2);
+	}
+}
+
+def Post(url, data){
+	post = new HttpPost(url)
+	if (data.class == String)
+		post.setEntity(new StringEntity("hello world"))
+	else if (java.io.InputStream.isInstance(data))
+		post.setEntity(new InputStreamEntity(data))
+	else
+		println "unknown data type: "+data.class
+	return Execute(post)
+}
+
+
+
+
+data = new ByteArrayInputStream("abcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfklabcdfasdfasdfasdflkjaskldfjaksldfklasdfdjfkalsdfklasjdfklasjdfkl".bytes)
+println "abcd".bytes
+println data
+println Post("http://172.16.0.170:1122", data)
+
+
+
